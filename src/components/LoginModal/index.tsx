a33a7ff2ba66a09f09c970e2154d3e6acc9c8079
Phạm Thanh Huy <huyp7922@gmail.com>
2025-11-16 21:49:32 +0700

    style(login): update login modal colors
    - Changed background color
    - Changed text color
    - Changed button styles

--- src/components/LoginModal/index.tsx
+++ src/components/LoginModal/index.tsx
@@ -25,34 +25,34 @@ const AuthSocialModal: React.FC<AuthSocialModalProps> = ({
   iconGradient = "from-pink-500 via-red-500 to-yellow-400",
 }) => {
   const { t } = useTranslation();
-  
+
   return (
     <ModalPopup customModalClassName="w-120" onClose={onClose}>
-      <Card className="bg-neutral-900  border-none text-center rounded-2xl shadow-xl">
+      <Card className="bg-primary-foreground border-none text-center rounded-2xl shadow-xl">
         <CardContent className="p-6 space-y-5">
           <div className={`flex justify-center ${Icon ? "" : "hidden"}`}>
             <div
               className={`w-20 h-20 rounded-lg bg-gradient-to-from ${iconGradient} flex items-center justify-center`}
             >
-              {Icon && <Icon size={30} className="text-white w-12 h-12" />}
+              {Icon && <Icon size={30} className="text-foreground w-12 h-12" />}
             </div>
           </div>
 
-          <h2 className="text-white font-bold text-3xl">
+          <h2 className="text-foreground font-bold text-3xl">
             {title || t("authModal.signUpToPost")}
           </h2>
-          <p className="text-gray-400 text-sm leading-relaxed ">
+          <p className="text-foreground text-sm leading-relaxed ">
             {description || t("modal.joinThreads")}
           </p>
 
           <Button
             onClick={onContinue}
-            className="w-full bg-neutral-800 hover:bg-neutral-700 text-white justify-between py-6 rounded-xl"
+            className="w-full bg-primary-foreground border cursor-pointer hover:!bg-transparent text-foreground justify-between py-6 rounded-xl"
           >
             <div onClick={onContinue} className="flex items-center space-x-3">
               <span>{buttonText || t("authModal.continueWithLogin")}</span>
             </div>
-            <span className="text-gray-400">{">"}</span>
+            <span className="text-foreground">{">"}</span>
           </Button>
         </CardContent>
       </Card>
